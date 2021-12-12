bbd955d269d66b78e888d63d412631f2a9653f4d
Kazuho-Shibuya <businessfive25@gmail.com>
2021-12-13 03:24:34 +0900

    extendにjsx-a11yを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     },
     "extends": [
         "plugin:react/recommended",
-        "plugin:react-hooks/recommended"
+        "plugin:react-hooks/recommended",
+        "plugin:jsx-a11y/recommended"
     ],
     "parserOptions": {
         "sourceType": "module"
