a138fbcdf583a9078428a28a2098e5592e3d87d1
Kazuho-Shibuya <businessfive25@gmail.com>
2022-01-01 21:29:52 +0900

    indexページにHeadlinesコンポーネントを追加

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { Footer } from '../components/Footer';
 import Links from '../components/Links';
+import Headlines from '../components/Headlines';
 
 export default function Home() {
   return (
@@ -14,11 +15,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Index Page</h1>
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
+        <Headlines />
         <Links />
       </main>
       <Footer />
