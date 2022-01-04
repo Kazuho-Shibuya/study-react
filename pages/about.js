f04b8c3a6cc67971a03a27a0f1bc17ed897be7b5
Kazuho-Shibuya <businessfive25@gmail.com>
2022-01-05 04:02:42 +0900

    headlinesコンポーネントに渡す引数を追加

--- pages/about.js
+++ pages/about.js
@@ -14,7 +14,7 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main className={styles.main}>
-        <Headlines title='About Page' />
+        <Headlines title='About Page' page='about' />
         <Links />
       </main>
       <Footer />
