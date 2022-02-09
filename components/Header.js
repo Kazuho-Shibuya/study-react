a3c2f87e76a33502c3849f1af50740aaa59f0faf
Kazuho-Shibuya <businessfive25@gmail.com>
2022-02-10 02:14:26 +0900

    Header.jsの修正

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
-import styles from './Header.module.css';
+// import styles from './Header.module.css';
 
 export function Header() {
   return (
-    <header className={styles.header}>
-      <a
-        href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
+    <header>
+      <a href=''>
         Powered by{' '}
-        <span className={styles.logo}>
+        <span>
           <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
         </span>
       </a>
