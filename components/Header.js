840a23f77a8520456df63dce07746a2a45027078
Kazuho-Shibuya <businessfive25@gmail.com>
2022-02-11 06:54:09 +0900

    Headerコンポーネントの修正

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import Image from 'next/image';
 // import styles from './Header.module.css';
 
 export function Header() {
   return (
     <header>
-      <a href=''>
-        Powered by{' '}
-        <span>
-          <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-        </span>
-      </a>
+      <a href='/'>Index</a>
+      <a href='/about'>About</a>
     </header>
   );
 }
