a06712291ff33c9963dd63dfee2ae4bc2474c8ae
Kazuho-Shibuya <businessfive25@gmail.com>
2022-02-21 06:36:50 +0900

    ITEMSを追加

--- components/Links.js
+++ components/Links.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import styles from './Link.module.css';
 
+const ITEMS = [
+  {
+    href: 'https://nextjs.org/docs',
+    title: 'Documentation &rarr;',
+    discription: 'Find in-depth information about Next.js features and API.',
+  },
+  {
+    href: 'https://nextjs.org/learn',
+    title: 'Learn &rarr;',
+    discription: 'Learn about Next.js in an interactive course with quizzes!',
+  },
+  {
+    href: 'https://github.com/vercel/next.js/tree/master/examples',
+    title: 'Examples &rarr;',
+    discription: 'Discover and deploy boilerplate example Next.js projects.',
+  },
+  {
+    href: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+    title: 'Deploy &rarr;',
+    discription: 'Instantly deploy your Next.js site to a public URL with Vercel.',
+  },
+];
+
 export function Links() {
   return (
     <div className={styles.grid}>
