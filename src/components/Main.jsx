84c963f556de2fa5596acf43c75ed644ac8af8e3
Kazuho-Shibuya <businessfive25@gmail.com>
2022-03-04 00:19:05 +0900

    Headlineディレクトリを追加

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Main.module.css';
 import { Links } from './Links';
-import { Headlines } from './Headlines';
+import { Headlines } from './Headline';
 
 export function Main(props) {
   return (
