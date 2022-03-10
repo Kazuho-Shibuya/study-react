10514bc2520a88db3f8f9738f8899c56653c05e6
Kazuho-Shibuya <businessfive25@gmail.com>
2022-03-11 02:12:36 +0900

    相対パスを絶対パスに変更

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import { Header } from '../components/Header';
-import { Footer } from '../components/Footer';
-import { Main } from '../components/Main';
+import styles from 'src/styles/Home.module.css';
+import { Header } from 'src/components/Header';
+import { Footer } from 'src/components/Footer';
+import { Main } from 'src/components/Main';
 
 export default function Home() {
   return (
