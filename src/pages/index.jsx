daa5cd9dbf1c49cf5ab78ff147aa746d12130ae8
Kazuho-Shibuya <businessfive25@gmail.com>
2022-04-06 02:48:18 +0900

    useCallbackを追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Head from 'next/head';
 import styles from 'src/styles/Home.module.css';
 import { Header } from 'src/components/Header';
@@ -7,9 +7,14 @@ import { Main } from 'src/components/Main';
 
 export default function Home() {
   const [count, setCount] = useState(1);
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(
+    (e) => {
+      if (count < 10) {
+        setCount((count) => count + 1);
+      }
+    },
+    [count],
+  );
 
   useEffect(() => {
     document.body.style.backgroundColor = 'lightblue';
@@ -28,7 +33,7 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header />
-      <h1>{foo}</h1>
+      <h1>{count}</h1>
       <button href='/about' onClick={handleClick}>
         ボタン
       </button>
