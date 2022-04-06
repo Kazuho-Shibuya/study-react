1a1dcb3d77209334a9d7ad8c2b9395a9994e4677
Kazuho-Shibuya <businessfive25@gmail.com>
2022-04-07 01:37:20 +0900

    不要な記述を削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,14 +7,11 @@ import { Main } from 'src/components/Main';
 
 export default function Home() {
   const [count, setCount] = useState(1);
-  const handleClick = useCallback(
-    (e) => {
-      if (count < 10) {
-        setCount((count) => count + 1);
-      }
-    },
-    [count],
-  );
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
 
   useEffect(() => {
     document.body.style.backgroundColor = 'lightblue';
