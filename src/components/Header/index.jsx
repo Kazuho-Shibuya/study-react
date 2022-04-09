041a0323c1c9e390f647031d84dea5f354d5a210
Kazuho-Shibuya <businessfive25@gmail.com>
2022-04-09 09:01:38 +0900

    ヘッダーのリンクをボタンに修正

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,10 +6,10 @@ export function Header() {
   return (
     <header className={styles.header}>
       <Link href='/'>
-        <a className={styles.anchor}>Index</a>
+        <button className={styles.anchor}>Index</button>
       </Link>
       <Link href='/about'>
-        <a className={styles.anchor}>About</a>
+        <button className={styles.anchor}>About</button>
       </Link>
     </header>
   );
