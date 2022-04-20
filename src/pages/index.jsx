cc92dcaa853154c3784ebbfc0a40919090cfe11e
Kazuho-Shibuya <businessfive25@gmail.com>
2022-04-20 21:54:13 +0900

    非表示ボタンでカウントを非表示にする

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,6 +8,7 @@ import { Main } from 'src/components/Main';
 export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState('');
+  const [isShow, setIsShow] = useState(true);
   const handleClick = useCallback(() => {
     if (count < 10) {
       setCount((count) => count + 1);
@@ -40,9 +41,16 @@ export default function Home() {
       </Head>
       <Header />
       <div className={styles.count}>
-        <h1>{count}</h1>
+        {isShow ? <h1>{count}</h1> : null}
         <button href='/about' onClick={handleClick}>
           ボタン
+        </button>
+        <button
+          onClick={() => {
+            setIsShow(false);
+          }}
+        >
+          非表示
         </button>
         <input type='text' value={text} onChange={handleChange} />
       </div>
