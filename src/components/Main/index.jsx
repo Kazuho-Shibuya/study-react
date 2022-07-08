bea3c8882ddf4f811b69abf80c9ba6dbf6d5a381
Kazuho-Shibuya <businessfive25@gmail.com>
2022-07-09 01:21:09 +0900

    不要な記載を削除

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,16 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Headlines } from 'src/components/Headline';
 import { Links } from 'src/components/Link';
 import styles from 'src/components/Main/Main.module.css';
 
 export function Main(props) {
-  useEffect(() => {
-    document.body.style.backgroundColor = 'red';
-    return () => {
-      document.body.style.backgroundColor = '';
-    };
-  }, []);
-
   return (
     <main className={styles.main}>
       <Headlines page={props.page}>
