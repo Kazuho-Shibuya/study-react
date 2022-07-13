9376ddd8d859578c4f39ea62824ffc6b4feada7e
Kazuho-Shibuya <businessfive25@gmail.com>
2022-07-13 22:29:52 +0900

    itemsをuseStateで管理

--- src/components/Link/index.jsx
+++ src/components/Link/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from 'src/components/Link/Link.module.css';
 
 const ITEMS = [
@@ -25,9 +25,11 @@ const ITEMS = [
 ];
 
 export function Links() {
+  const [items, setItems] = useState(ITEMS);
+
   return (
     <div className={styles.grid}>
-      {ITEMS.map((item) => {
+      {items.map((item) => {
         return (
           <a key={item.href} href={item.href} className={styles.card}>
             <h2 className={styles.title}>{item.title}</h2>
