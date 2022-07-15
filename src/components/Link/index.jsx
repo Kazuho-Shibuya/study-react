f80397c6035e0fec32c272d7ba418da0d564614b
Kazuho-Shibuya <businessfive25@gmail.com>
2022-07-15 23:22:43 +0900

    items,handleReduceのリフトアップ

--- src/components/Link/index.jsx
+++ src/components/Link/index.jsx
@@ -1,37 +1,7 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import styles from 'src/components/Link/Link.module.css';
 
-const ITEMS = [
-  {
-    href: 'https://nextjs.org/docs',
-    title: 'Documentation →',
-    discription: 'Find in-depth information about Next.js features and API.',
-  },
-  {
-    href: 'https://nextjs.org/learn',
-    title: 'Learn →',
-    discription: 'Learn about Next.js in an interactive course with quizzes!',
-  },
-  {
-    href: 'https://github.com/vercel/next.js/tree/master/examples',
-    title: 'Examples →',
-    discription: 'Discover and deploy boilerplate example Next.js projects.',
-  },
-  {
-    href: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
-    title: 'Deploy →',
-    discription: 'Instantly deploy your Next.js site to a public URL with Vercel.',
-  },
-];
-
-export function Links() {
-  const [items, setItems] = useState(ITEMS);
-  const handleReduce = useCallback(() => {
-    setItems((prevItems) => {
-      return prevItems.slice(0, prevItems.length - 1);
-    });
-  }, []);
-
+export function Links({ items, handleReduce }) {
   return (
     <div className={styles.grid}>
       <button onClick={handleReduce}>減らす</button>
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Headlines } from 'src/components/Headline';
 import { Links } from 'src/components/Link';
 import styles from 'src/components/Main/Main.module.css';
-
+const ITEMS = [
+  {
+    href: 'https://nextjs.org/docs',
+    title: 'Documentation →',
+    discription: 'Find in-depth information about Next.js features and API.',
+  },
+  {
+    href: 'https://nextjs.org/learn',
+    title: 'Learn →',
+    discription: 'Learn about Next.js in an interactive course with quizzes!',
+  },
+  {
+    href: 'https://github.com/vercel/next.js/tree/master/examples',
+    title: 'Examples →',
+    discription: 'Discover and deploy boilerplate example Next.js projects.',
+  },
+  {
+    href: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+    title: 'Deploy →',
+    discription: 'Instantly deploy your Next.js site to a public URL with Vercel.',
+  },
+];
 export function Main(props) {
+  const [items, setItems] = useState(ITEMS);
+  const handleReduce = useCallback(() => {
+    setItems((prevItems) => {
+      return prevItems.slice(0, prevItems.length - 1);
+    });
+  }, []);
+
   return (
     <main className={styles.main}>
       <Headlines page={props.page}>
         <code className={styles.code}>pages/{props.page}.js</code>
       </Headlines>
-      <Links />
+      <Links items={items} handleReduce={handleReduce} />
     </main>
   );
 }
