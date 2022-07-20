7f48b183f078b30e59eb9613226b6de27a0914c6
Kazuho-Shibuya <businessfive25@gmail.com>
2022-07-20 22:17:03 +0900

    ページによって背景色を変更する

--- src/hooks/useBgColorBlue.jsx
+++ src/hooks/useBgColorBlue.jsx
@@ -1,12 +1,14 @@
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 export const useBgColorBlue = () => {
+  const router = useRouter();
   useEffect(() => {
-    document.body.style.backgroundColor = 'lightblue';
+    document.body.style.backgroundColor = router.pathname === '/' ? 'lightblue' : 'beige';
     return () => {
       document.body.style.backgroundColor = '';
     };
-  }, []);
+  }, [router.pathname]);
 
   return {};
 };
