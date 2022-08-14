7bff2e0cc78906b99ed6a8794392b1c5b54263af
Kazuho-Shibuya <businessfive25@gmail.com>
2022-08-14 16:33:11 +0900

    propsの分割代入宣言を止める

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,9 +6,6 @@ import { Footer } from 'src/components/Footer';
 import { Main } from 'src/components/Main';
 
 const About = (props) => {
-  const { doubleCount, isShow, handleClick, handleDisplay, text, array, handleChange, handleAdd } =
-    props;
-
   return (
     <div className={styles.container}>
       <Head>
@@ -18,15 +15,15 @@ const About = (props) => {
       </Head>
       <Header />
       <div className={styles.count}>
-        {isShow ? <h1>{doubleCount}</h1> : null}
-        <button href='/about' onClick={handleClick}>
+        {props.isShow ? <h1>{props.doubleCount}</h1> : null}
+        <button href='/about' onClick={props.handleClick}>
           ボタン
         </button>
-        <button onClick={handleDisplay}>{isShow ? '非表示' : '表示'}</button>
-        <input type='text' value={text} onChange={handleChange} />
-        <button onClick={handleAdd}>追加</button>
+        <button onClick={props.handleDisplay}>{props.isShow ? '非表示' : '表示'}</button>
+        <input type='text' value={props.text} onChange={props.handleChange} />
+        <button onClick={props.handleAdd}>追加</button>
         <ul>
-          {array.map((item) => {
+          {props.array.map((item) => {
             return <li key={item}>{item}</li>;
           })}
         </ul>
