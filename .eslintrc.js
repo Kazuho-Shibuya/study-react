a6efd734e712998a9f1c40cc29893fe954447c5f
Kazuho-Shibuya <businessfive25@gmail.com>
2022-08-25 16:56:34 +0900

    next.js用のeslintを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
     'prettier',
+    'plugin:@next/next/recommended',
   ],
   parserOptions: {
     sourceType: 'module',
