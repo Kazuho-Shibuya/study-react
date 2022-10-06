a1459c76c9504c94075cbb5bfeb881d10b1befd5
Kazuho-Shibuya <businessfive25@gmail.com>
2022-10-06 23:57:00 +0900

    swrでuseReducerを書き換える

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,67 +1,71 @@
-import React, { useCallback, useEffect, useReducer } from 'react';
+import useSWR from 'swr';
 
-const initisalState = {
-  data: [],
-  loading: true,
-  error: false,
-};
+import React from 'react';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'end':
-      return {
-        ...state,
-        data: action.data,
-        loading: false,
-      };
-    case 'error':
-      return {
-        ...state,
-        error: action.error,
-        loading: false,
-      };
+// const initisalState = {
+//   data: [],
+//   loading: true,
+//   error: false,
+// };
 
-    default: {
-      throw new Error('no such action type');
-    }
-  }
-};
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case 'end':
+//       return {
+//         ...state,
+//         data: action.data,
+//         loading: false,
+//       };
+//     case 'error':
+//       return {
+//         ...state,
+//         error: action.error,
+//         loading: false,
+//       };
+
+//     default: {
+//       throw new Error('no such action type');
+//     }
+//   }
+// };
 
 export const Posts = () => {
-  const [state, dispatch] = useReducer(reducer, initisalState);
+  const { data, error } = useSWR('https://jsonplaceholder.typicode.com/posts');
 
-  const getPosts = useCallback(async () => {
-    try {
-      const res = await fetch('https://jsonplaceholder.typicode.com/postsaaa');
-      if (!res.ok) {
-        throw new Error('エラーが発生したため、データを取得できませんでした');
-      }
-      const json = await res.json();
-      dispatch({ type: 'end', data: json });
-    } catch (error) {
-      dispatch({ type: 'error', error });
-    }
-  }, []);
+  // const [state, dispatch] = useReducer(reducer, initisalState);
 
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
+  // const getPosts = useCallback(async () => {
+  //   try {
+  //     const res = await fetch('https://jsonplaceholder.typicode.com/postsaaa');
+  //     if (!res.ok) {
+  //       throw new Error('エラーが発生したため、データを取得できませんでした');
+  //     }
+  //     const json = await res.json();
+  //     dispatch({ type: 'end', data: json });
+  //   } catch (error) {
+  //     dispatch({ type: 'error', error });
+  //   }
+  // }, []);
 
-  if (state.loading) {
+  // useEffect(() => {
+  //   getPosts();
+  // }, [getPosts]);
+
+  if (!error && !data) {
     return <div>ローディング中</div>;
   }
 
-  if (state.error) {
-    return <div>{state.error.message}</div>;
+  if (error) {
+    return <div>{error.message}</div>;
   }
 
-  if (state.data.length === 0) {
+  if (data.length === 0) {
     return <div>データは空です</div>;
   }
 
   return (
     <ol>
-      {state.data.map((post) => {
+      {data.map((post) => {
         return <li key={post.id}>{post.title}</li>;
       })}
     </ol>
