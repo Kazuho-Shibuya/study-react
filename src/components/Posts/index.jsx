9bdeb7a92cde646a724fe8900666c2c8bc43b8c8
Kazuho-Shibuya <businessfive25@gmail.com>
2022-10-23 11:49:49 +0900

    記述位置の修正

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,18 +1,18 @@
 import useSWR from 'swr';
 import React from 'react';
 
+const fetcher = async (url) => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error('エラーが発生したため、データの取得に失敗しました');
+  }
+
+  const json = await response.json();
+  return json;
+};
+
 export const Posts = () => {
-  const fetcher = async (url) => {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error('エラーが発生したため、データの取得に失敗しました');
-    }
-
-    const json = await response.json();
-    return json;
-  };
-
   const { data, error } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher);
 
   if (!error && !data) {
