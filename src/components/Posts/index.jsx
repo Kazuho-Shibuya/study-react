a4ecc489bd8ece1bcd95f59971213550ee3d9d80
Kazuho-Shibuya <businessfive25@gmail.com>
2022-11-03 15:19:24 +0900

    usePostsをフックにまとめる

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,26 +1,5 @@
-import useSWR from 'swr';
+import { usePosts } from 'src/hooks/usePosts';
 import React from 'react';
-
-const fetcher = async (url) => {
-  const response = await fetch(url);
-
-  if (!response.ok) {
-    throw new Error('エラーが発生したため、データの取得に失敗しました');
-  }
-
-  const json = await response.json();
-  return json;
-};
-
-const usePosts = () => {
-  const { data, error } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher);
-  return {
-    data,
-    error,
-    isLoading: !error && !data,
-    isEmpty: data && data.length === 0,
-  };
-};
 
 export const Posts = () => {
   const { data, error, isLoading, isEmpty } = usePosts();
